b116fdb8d6e80a63953164a7b2d4753b60d42529
Charles P <charles.prystupa@gmail.com>
2020-10-26 19:25:34 +0000

    Created an assert script to make a more verbose debugger

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  let passEmoji = String.fromCodePoint(0x2705); // Green Check
+  let failEmoji = String.fromCodePoint(0x26D4); // No Entry Emoji
+
+  if (actual === expected) {
+    console.log(passEmoji + "> Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(failEmoji + "> Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Computer", "Computer");
+assertEqual(1, 1);
+assertEqual(4, 1);
