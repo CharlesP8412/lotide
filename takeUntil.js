a5112c4f7a0a1ba6e4144e3c3f139993b447a82f
Charles P <charles.prystupa@gmail.com>
2020-10-29 19:41:41 +0000

    takeUntil, slices array until a condition is hit (Using CALLBACKS)

---
+++ takeUntil.js
@@ -0,0 +1,40 @@
+/* 
+Take 2 parameters
+Array to work with
+The Callback 
+*/
+
+
+const takeUntil = function (array, callback) {
+  let results = []
+
+  for (let item of array) {
+    //CONDITION TRUE
+    if (callback(item) === true) {
+      return results
+    } else {
+      results.push(item)
+      //Do this until hit the TRUE w. Callback
+    }
+  }
+  return results
+}
+
+// INPUT
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);   // Goes until it hits something < 0
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');  //Goes Until it hits a lone comma ,
+console.log(results2);
+
+
+/*
+[ 1, 2, 5, 7, 2 ]
+---
+[ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
+*/
