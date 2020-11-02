90b66fc90fe1712e39124f7432ae8993dc7aa20f
Charles P <charles.prystupa@gmail.com>
2020-11-02 20:45:12 +0000

    cleaned up middleTest

--- test/_template.js
+++ test/_template.js
@@ -1,8 +1,10 @@
+
+
 const assert = require('chai').assert;
 // const head = require('../head.js');
 
 describe("#INSERT NAME", () => {
-  
+
   it("", () => {
     assert.strictEqual();
   });
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,31 +1,20 @@
 const middle = require('../middle');
 const assert = require('chai').assert;
-// const assertArraysEqual = require('../assertArraysEqual');
 
-
-
-// assertArraysEqual(test1, [])
-// assertArraysEqual(test2, [])
-// assertArraysEqual(test3, [2])
-// assertArraysEqual(test4, [3])
-// assertArraysEqual(test5, [2, 3])
-// assertArraysEqual(test6, [3, 4])
 
 describe("#middle", () => {
 
   const test1 = middle([1]) // => []
   const test2 = middle([1, 2]) // => []
-  const test3 = middle([1, 2, 3]) // => [2]
-  const test4 = middle([1, 2, 3, 4, 5]) // => [3]
-  const test5 = middle([1, 2, 3, 4]) // => [2, 3]
-  const test6 = middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+  const test3 = middle([1, 2, 3, 4, 5]) // => [3]
+  const test4 = middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
 
   it("return middle of an array w/ EVEN elements", () => {
-    assert.deepStrictEqual(test6, [3, 4]);
+    assert.deepStrictEqual(test4, [3, 4]);
   });
 
   it("return middle of an array w/ ODD elements", () => {
-    assert.deepEqual(test4, [3]);
+    assert.deepEqual(test3, [3]);
   });
 
   it("return empty array with given an array with ONE element [1]", () => {
