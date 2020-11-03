d0809b5d4cc11dc7154f16b8fcd2744fa6ccdf77
Charles P <charles.prystupa@gmail.com>
2020-11-03 02:34:54 +0000

    WiP: flatten works for basic arrays, need to incorporate recursion for more complexity

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const assertArraysEqual = require("./assertArraysEqual");
+const eqArrays = require("./eqArrays");
+
+// flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
+
+function flatten(array) {
+  let newArray = [];
+  array.forEach(item => {
+    if (Array.isArray(item)) {
+      if (item.length > 0) {
+        for (subItem of item) {
+          newArray.push(subItem)
+        }
+      } else {
+        flatten(item);
+      }
+    } else {
+      newArray.push(item);
+    }
+  });
+  return newArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
+
+let test1 = [1, 2, 3, 4, 5];
+let test2 = [[1, 2, [[3], 4]], 5, []];
+let test3 = [[[[[[[[[[[[[1]]]]]]]]]]]]];
+let testx = [2, 1, [3, 4], 3];
+console.log(">> ", flatten(test1));
+console.log(">> ", flatten(test2));
+console.log(">> ", flatten(test3));
+console.log(">> ", flatten(testx));
